fc68e3c0180b6e1625b296013d51ee01a214ae6d
Naveen <naveenofficial7@gmail.com>
2020-06-06 16:07:41 +0530

    MavenProject Commit

---
+++ src/main/java/Testjava.java
@@ -0,0 +1,14 @@
+class test1{
+	public void testing(){
+	System.out.println("objectFile");
+	}
+}
+
+class Testjava {
+		public static void main ( String[] args) {
+		    test1 tester = new test1();
+		    tester.testing();
+	   System.out.println ("TestJavaFile");
+}
+}
+
